121a51d6b609a6e90b907a560f3a094515295ddf
TalicoInitative <jauharatmik@gmail.com>
2026-06-21 20:39:07 -0700

    Update fala-comigo-deploy/vite.config.js

--- fala-comigo-deploy/vite.config.js
+++ fala-comigo-deploy/vite.config.js
@@ -7,5 +7,18 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:3001'
     }
+  },
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'data-convos': ['./src/data/convos.js'],
+          'data-stories': ['./src/data/stories.js'],
+          'data-words': ['./src/data/words.js'],
+          'data-other': ['./src/data/grammar.js', './src/data/sentences.js', './src/data/crossunit.js', './src/data/biaskills.js', './src/data/connections.js'],
+        }
+      }
+    }
   }
 });
